58d827ef630d485fd20eb4e3a1985a26fb1bd0a5
igor-ribeiiro <igormr98mr@gmail.com>
2019-12-01 18:24:45 -0300

    Script should be done

--- run.py
+++ run.py
@@ -10,16 +10,12 @@ if __name__ == "__main__":
     f.write(str(candidate))
     f.close()
 
-    string_params = ""
     for i in range(n_proccess):
-        tam_i = len(str(i))
-        string_params += ":8"
-        for j in range(3-tam_i):
-            string_params += "0"
-        num = int(i)
-        for j in range(tam_i):
-            string_params += int(str(num)[0])
-            num = int(num/10)
+        command = "x-terminal-emulator -e go run main.go " + str(i+1) + " "
+        if i+1 == candidate:
+            command += "1"
+        else:
+            command += "0"
 
-    for i in range(n_proccess):
-        os.system("x-terminal-emulator -e go run main.go")
+        print(command)
+        os.system(command)
